86b265cd0cb0aa23c19d0c4e5b92a1b560eb6c33
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-02-17 08:01:39 -0800

    print alphabets using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets using putchar
+ *
+ * Description: This program will prints the alphabets using
+ *              the `putchar` function
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
