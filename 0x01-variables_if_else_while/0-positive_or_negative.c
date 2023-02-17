5b4d35ecc7cf88c52369143bbd9b528e5d14a082
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-02-17 06:11:34 -0800

    adhere to betty styling

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,6 +5,9 @@
 /**
  * main - determine if a rand av. is +, -, or 0
  *
+ * Description: Given a random variable this program will determine
+ * if this variable is +, -, or 0
+ * Return: returns 0
  */
 int main(void)
 {
@@ -14,15 +17,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
 	{
-		printf("%d is negative \n", n);
+		printf("%d is negative\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero \n", n);
+		printf("%d is zero\n", n);
 	}
-	else 
+	else
 	{
-		printf("%d is positive \n", n);
+		printf("%d is positive\n", n);
 	}
 	return (0);
 }
