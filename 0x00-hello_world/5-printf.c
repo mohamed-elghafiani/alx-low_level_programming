7f1004683595475a76dae07defb78070c7b58d70
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-02-16 17:40:25 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - prints a message using printf
+ *
+ * Description: This function will use the printf function to print a message
+ * Return: returns 0 if everything went good!
+ */
+int main(void)
+{
+printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+}
