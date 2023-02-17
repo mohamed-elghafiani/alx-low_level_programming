8107598785307453c1bb8b16474c51fcd7f0202b
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-02-17 03:17:13 -0800

    print to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - peint a MSG to stderr
+ *
+ * Description: This function will print a MSG to stderr
+ * Return: returns 0 if everything went good!
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+return (1);
+}
