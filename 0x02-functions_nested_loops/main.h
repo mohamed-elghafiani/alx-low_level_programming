c7fe288d6b18f24cf2abc4e26e918d994bf27269
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-02-22 13:51:17 -0800

    check if character is lowercase

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,9 +3,12 @@
 int _putchar(char c);
 
 /* a function that prints the alphabet, 
- * in lowercase, followed by a new line.*/
+ * in lowercase, followed by a new line */
 void print_alphabet(void);
 
 /* function that prints 10 times the alphabet, 
- * in lowercase, followed by a new line.*/
+ * in lowercase, followed by a new line */
 void print_alphabet_x10(void);
+
+/* a function that checks for lowercase character */
+int _islower(int c);
