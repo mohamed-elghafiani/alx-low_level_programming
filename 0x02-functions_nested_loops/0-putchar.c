9f031fbb13f688f1ba8520ca61b53c0c607b493f
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-02-21 16:32:39 -0800

    printing _putchar using _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "_putchar.c"
+/**
+ * main - a program that prints "_putchar", followed by a new line.
+ *
+ * Return: return on success 0!
+ *
+ */
+
+int main(void)
+{
+	char str[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(str[i]);
+		
+		i++;
+	}
+	
+	_putchar('\n');
+	return (0);
+}
