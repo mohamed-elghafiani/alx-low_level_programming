34093ef02490e4d60918338e41fa46f20e181396
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-02-27 16:28:45 -0800

    print an array of integers - fixed!

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,7 +11,7 @@
 void print_array(int *a, int n)
 {
 	int i;
-	
+
 	if (n > 0)
 	{
 		for (i = 0; i < n - 1; i++)
@@ -19,5 +19,9 @@ void print_array(int *a, int n)
 			printf("%d, ", a[i]);
 		}
 		printf("%d\n", a[i]);
+	}
+	else
+	{
+		printf("\n");
 	}
 }
