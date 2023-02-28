8c3679cf0cbf037eb4a0a4187089812c19056e82
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-02-27 17:25:45 -0800

    reverse string - got it! maybe

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * rev_string - a function that reverse a string
@@ -12,24 +13,20 @@
 void rev_string(char *s)
 {
 	int len_s = 0, i;
-	char *tmp = "";
+	char *tmp = NULL;
 
+	tmp = (char *) malloc(strlen(s) + 1);
 	strcpy(tmp, s);
-	
-	printf("this is string: %s\n", s);
 
 	while (*(s + len_s) != '\0')
 	{
 		len_s++;
-	} 
+	}
 
 	for (i = 0; i < len_s; i++)
 	{
 		s[i] = tmp[len_s - i - 1];
 	}
 
-
-	printf("this is string: %s\n", s);
-	printf("this is tmp: %s\n", tmp);
-
+	free(tmp);
 }
