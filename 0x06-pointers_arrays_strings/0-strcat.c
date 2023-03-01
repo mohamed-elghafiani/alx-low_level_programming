de4cb0da815a88b5bde8398a150ca1d8a4bce33e
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-03-01 02:53:20 -0800

    concatenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+/**
+ * _strcat - a function that concatenate two strings
+ *
+ * @dest: The destination string which we will append to
+ * @src: The source string that will be appended to @dest
+ * Return: returns a pointer to the resulting string @dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+	
+	/* Destermining where @src should be append */
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	/* appending @src to @dest */
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
+}
