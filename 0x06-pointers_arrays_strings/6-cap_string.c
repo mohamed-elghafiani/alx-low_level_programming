373f3166ce578910b7ad9fe559869446f2c75038
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-03-01 13:19:58 -0800

    capitalize string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+/**
+ * cap_string - a function that capitalizes all words of a string
+ *
+ * @str: the string to capitalize
+ * Return: returns a pointer to the resulted string
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0, j;
+	int is_sep = 0;
+	char sep[13] = {'\t', ' ', '\n', ',', ';', '.', '!', '?', '"', '('};
+
+	sep[10] = ')';
+	sep[11] = '{';
+	sep[12] = '}';
+
+	while (str[i] != '\0')
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (str[i] == sep[j] && is_sep != 1)
+			{
+				is_sep = 1;
+			}
+		}
+
+		if (is_sep == 1 && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+		{
+			str[i + 1] = str[i + 1] + ('A' - 'a');
+		}
+
+		i++;
+		is_sep = 0;
+	}
+	return (str);
+
+
+}
