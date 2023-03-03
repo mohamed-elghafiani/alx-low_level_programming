198bf6357b9921b6cfc0f7900542d0cc3dfb4e8a
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-03-03 05:47:40 -0800

    print_numbers function

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,10 +9,15 @@
 
 void print_number(int n)
 {
-	int n_10 = 1, tmp = n;
+	int n_10 = 1, tmp = n, is_INT_MIN = 0;
 
 	if (n < 0)
 	{
+		if (n == -2147483648)
+		{
+			n++;
+			is_INT_MIN = 1;
+		}
 		n = -n;
 		_putchar('-');
 	}
@@ -20,7 +25,7 @@ void print_number(int n)
 	while (tmp / 10 != 0)
 	{
 		n_10 *= 10;
-		tmp /= 10; 
+		tmp /= 10;
 	}
 
 	while (n_10 > 1)
@@ -28,6 +33,10 @@ void print_number(int n)
 		_putchar(n / n_10 + '0');
 		n = n % n_10;
 		n_10 /= 10;
+	}
+	if (is_INT_MIN)
+	{
+		n++;
 	}
 	_putchar(n % 10 + '0');
 }
