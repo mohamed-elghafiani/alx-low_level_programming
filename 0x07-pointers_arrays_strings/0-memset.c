db76df3767e11ea0d84000b4ce478fcee09080c0
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-03-07 11:51:54 -0800

    memset function implementation

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+
+/**
+ * _memset - a function that fills memory with a constant byte
+ *
+ * @s: a pointer to the location of memory to fill
+ * @b: The constant byte to use for filling
+ * @n: The n furst bytes of fill
+ * Returns a pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+	return (s);
+}
