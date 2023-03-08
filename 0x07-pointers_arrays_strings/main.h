8f80b119c14dc24278ae40bff69954e270ff6cd6
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-03-08 04:43:38 -0800

    print_chessboard function

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
 
 char *_memset(char *s, char b, unsigned int n);
 
@@ -13,6 +14,8 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
+
+void print_chessboard(char (*a)[8]);
 
 
 #endif
