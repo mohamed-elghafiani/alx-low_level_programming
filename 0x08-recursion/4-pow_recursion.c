4cb2c2d3e03146cb9295068f6433734370335bc8
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-03-08 14:20:48 -0800

    pow_recusrion function

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+
+/**
+ * _pow_recursion - a function that returns the value of
+ *	x raised to the power of y
+ *
+ * @x: the integer
+ * @y: the exponent
+ * Return: returns the value of x^(y)
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	if (y == 0)
+	{
+		return (1);
+	}
+	y--;
+	return (x * _pow_recursion(x, y--));
+}
