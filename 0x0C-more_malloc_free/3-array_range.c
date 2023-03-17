7afbc6bf8780a0c4feb456257eee63b2d8476abc
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-03-17 16:47:12 -0700

    coding an array_range function - fix return

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,11 @@ int *array_range(int min, int max)
 	int *arr = malloc(sizeof(int) * (max - min + 1));
 	int j;
 
-	if (min > max || arr == NULL)
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	if (min > max)
 	{
 		return (NULL);
 	}
