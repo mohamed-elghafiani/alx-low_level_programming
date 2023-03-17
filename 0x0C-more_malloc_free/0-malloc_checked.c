9a739428421288f25cf8e892145647b9c84e326c
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-03-17 16:57:45 -0700

    malloc_check function

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,12 +10,12 @@
 
 void *malloc_checked(unsigned int b)
 {
-	if (malloc(b) == NULL)
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
 	{
 		exit(98);
 	}
-	else
-	{
-		return (malloc(b));
-	}
+	return (ptr);
 }
