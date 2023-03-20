d406290aa13ce801295b07c093243bea1f46e2d8
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-03-20 16:54:42 -0700

    creating init_dog function - check for NULL ptr

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stddef.h>
 
 /**
  * init_dog - a function that initialise the dog structure
@@ -12,8 +13,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
 
