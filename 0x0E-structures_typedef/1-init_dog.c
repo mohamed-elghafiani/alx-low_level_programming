92ae915a6c90fbc601a92d89b9ec9590aace583f
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-03-20 16:43:11 -0700

    creating init_dog function - betty style

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,15 @@
 #include "dog.h"
 
+/**
+ * init_dog - a function that initialise the dog structure
+ *
+ * @d: a pointer to the dog structure
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Return: Nothing
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
