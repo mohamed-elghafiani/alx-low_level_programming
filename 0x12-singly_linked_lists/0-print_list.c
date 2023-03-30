5ec3e519226e0ee700a7d5bc699bdadca56ac451
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-03-30 15:35:36 -0700

    create a function that prints a linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * print_list - a function that prints all the elements of a list_t list.
+ *
+ * @list_t: a linked list
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int n = 0;
+	const list_t *tmp;
+
+	tmp = h;
+	while (tmp)
+	{
+		if (tmp->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", tmp->len, tmp->str);
+		}
+		tmp = tmp->next;
+		n++;
+	}
+	return (n);
+}
+
