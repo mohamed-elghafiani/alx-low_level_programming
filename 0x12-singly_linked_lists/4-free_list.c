bb8b95825d38692990ed37619472e9a6460790a5
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-03-30 18:18:26 -0700

    add a free_list function

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include <stddef.h>
+#include<stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - a function that frees a list_t list
+ *
+ * @head: head of the linked list
+ * Return: Nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp, *c;
+
+	tmp = head;
+	while (tmp->next != NULL)
+	{
+		c = tmp;
+		free(c->str);
+		free(c);
+		tmp = tmp->next;
+	}
+	free(tmp->str);
+	free(tmp);
+}
