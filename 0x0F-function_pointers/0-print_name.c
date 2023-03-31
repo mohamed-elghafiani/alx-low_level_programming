cd62a851532b0fda9dfa1649ee4532a9fdef62a7
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-03-31 05:41:28 -0700

    add a function that prints a name using a pointer to another function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+
+/**
+ * print_name - a function that prints a name
+ *
+ * @name: the name to print
+ * @f: a function pointer
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
