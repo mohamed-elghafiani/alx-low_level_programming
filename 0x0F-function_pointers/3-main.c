a5741b7e1446c8ab1465c81c9270f1aedeca2b6c
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-03-31 13:42:39 -0700

    add a program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,12 +14,21 @@ int main(int argc, char* argv[])
 {
 	int a, b;
 	char *s;
-	(void)argc;
+
+	if (argc <= 4)
+	{
+		printf("Error\n");
+		return (0);
+	}
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	s = argv[2];
-
+	if (get_op_func(s) == NULL)
+	{
+		printf("Error\n");
+		return (0);
+	}
 	printf("%d\n", get_op_func(s)(a, b));
 	return (0);
 }
