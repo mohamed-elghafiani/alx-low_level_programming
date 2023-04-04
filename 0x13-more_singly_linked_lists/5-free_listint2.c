6c8029c34cca697e406bafe6b29fd6ef009f2081
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-04-04 15:48:57 -0700

    task #5: added a function that frees the linked list and set the header pointer to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "lists.h"
+
+
+/**
+ * free_listint2 - a function that frees a listint_t list
+ *	and set the head to NULL
+ *
+ * @head: a pointer to the head of the linked list
+ * Return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head == NULL || *head == NULL)
+	{
+		return;
+	}
+	while ((*head)->next)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+	free(*head);
+	head = NULL;
+}
