67e100e1e8f50d91c6d1f6c0560a72a5e38037c7
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-04-07 19:15:55 -0700

    task #3: added a function that sets the value of a bit to 1 at a given index

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,6 +11,6 @@ void print_bits(unsigned long int n);
 
 int get_bit(unsigned long int n, unsigned int index);
 
-int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif
