7c5c94b512807848b250fbefad4263bc8a9df1b9
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-04-10 19:55:34 -0700

    task #1: added a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -34,7 +34,7 @@ int create_file(const char *filename, char *text_content)
 		close(o);
 		return (1);
 	}
-	w = write(o, text_content, sizeof(text_content));
+	w = write(o, text_content, sizeof(text_content) - 1);
 	if (w == -1)
 	{
 		close(o);
