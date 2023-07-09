e36d5598bd45d1fe03b197b59f877cefef68cc36
mohamed-elghafiani <medghafiani@gmail.com>
2023-07-09 13:08:25 -0700

    betty style fixed!

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -12,7 +12,7 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index, key_hash;
-	
+
 	key_hash = hash_djb2(key);
 	index = key_hash % size;
 
