953b2e15bbab9446af25f28d01ab2760337e8561
mohamed-elghafiani <medghafiani@gmail.com>
2023-07-11 06:01:12 -0700

    return NULL if table is empty

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,6 +14,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *tmp;
 
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
 	index = key_index((unsigned char *)key, ht->size);
 	tmp = ht->array[index];
 
